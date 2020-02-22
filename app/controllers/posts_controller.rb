3cc2449f4ac2897d787ecb326642c6b6bad69ecd
Syoma Iwauchi <syoma4645@gmail.com>
2020-02-22 17:49:04 +0900

    トップページをindexに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def index
+    @posts = Post.includes(:photos, :user).limit(10).order('created_at DESC')
+  end
+
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
